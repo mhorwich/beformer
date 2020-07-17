4c85de7df2053e94d5a434c86ead5e883e270ef8
Max Horwich <32074435+mhorwich@users.noreply.github.com>
2020-07-16 22:47:06 -0400

    overhaul

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+//Ripple Event Handler
+var drawRipple = function(ev) {
+  var x = ev.clientX;
+  var y = ev.clientY;
+  var node = document.querySelector(".ripple");
+  var newNode = node.cloneNode(true);
+  newNode.classList.add("animate");
+  newNode.style.left = ev.clientX - 5 + "px";
+  newNode.style.top = ev.clientY - 5 + "px";
+  node.parentNode.replaceChild(newNode, node);
+};
+
+var drawRipple2 = function(ev) {
+  var clientX = ev.touches[0].clientX;
+  var clientY = ev.touches[0].clientY;
+  
+  var node = document.querySelector(".ripple");
+  var newNode = node.cloneNode(true);
+  newNode.classList.add("animate");
+  newNode.style.left = clientX - 5 + "px";
+  newNode.style.top = clientY - 5 + "px";
+  node.parentNode.replaceChild(newNode, node);
+};
+
+
+//Ripple Triggers
+window.addEventListener("mousedown", drawRipple);
+window.addEventListener("touchstart", drawRipple2);
+
+
+let words = "click anywhere to play sound";
 let cx, cy;
 let secondsRadius;
 let minutesRadius;
 let hoursRadius;
 let clockDiameter;
-
 
 var pingpong2 = new Tone.PingPongDelay(0.9, 0.4).toMaster();
 pingpong2.wet = 0.5;
@@ -55,22 +85,20 @@ function setup() {
   let canvas = createCanvas(150, 150);
   
   canvas.parent("clock");
-  
+
   stroke(255);
 
   let radius = min(width, height) / 2;
   secondsRadius = radius * 0.71;
   minutesRadius = radius * 0.6;
   hoursRadius = radius * 0.5;
-  clockDiameter = radius * 1.7;
+  clockDiameter = radius * 1.5;
 
   cx = width / 2;
   cy = height / 2;
-
-  
 }
 
-function draw() {
+function draw() {  
   var amp = map(mouseX, 0, width, -60, 0);
 
   var vox1amp = map(sin(frameCount / 100), -1, 1, -50, -10);
@@ -87,15 +115,19 @@ function draw() {
   slide.volume.value = slideamp;
   delay.volume.value = delayamp;
 
-    
-  // Draw the clock background
+  
   noStroke();
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height){
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
     fill(255, 20);
   } else {
-   fill(37, 20);
+    fill(37, 20);
   }
-  ellipse(cx, cy, (clockDiameter + sin(frameCount)*10), (clockDiameter + sin(frameCount)*10));
+  ellipse(
+    cx,
+    cy,
+    clockDiameter + sin(frameCount) * 10,
+    clockDiameter + sin(frameCount) * 10
+  );
 
   // Angles for sin() and cos() start at 3 o'clock;
   // subtract HALF_PI to make them start at the top
@@ -104,10 +136,10 @@ function draw() {
   let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
 
   // Draw the hands of the clock
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height){
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
     stroke(37);
   } else {
-  stroke(255);    
+    stroke(255);
   }
   strokeWeight(1);
   line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
@@ -126,9 +158,6 @@ function draw() {
     vertex(x, y);
   }
   endShape();
-
-  
-  
 }
 
 function mousePressed() {
@@ -143,8 +172,7 @@ function mousePressed() {
     delay.start();
     started = true;
     Tone.Master.mute = false;
-    x.innerHTML = "<i>click anywhere to stop sound</i>";
-
+    x.innerHTML = "<p>click anywhere to stop sound</p>";
   } else {
     vox1.stop();
     vox2.stop();
@@ -154,7 +182,6 @@ function mousePressed() {
     delay.stop();
     started = false;
     Tone.Master.mute = true;
-    x.innerHTML = "<i>click anywhere to play sound</i>";
-
+    x.innerHTML = "<p>click anywhere to play sound</p>";
   }
 }
